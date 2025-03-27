4640373505e261c1fbd311906857468a8033a7db
TalanTabares <143756711+TalanTabares@users.noreply.github.com>
2025-03-26 22:23:16 -0500

    Header without inputs & outputs

---
+++ Problem1.c
@@ -0,0 +1,17 @@
+/*
+Author: Talan Tabares
+StudentID: 3108074
+Date: 3/26/2025
+Reason: Lab07
+Description:
+Program that calculates possible combinations of football scores based on the total score. The total score is an input from the user and all combinations are printed to terminal
+
+inputs:
+
+
+outputs:
+
+
+*/
+
+
---
+++ Problem2.c
@@ -0,0 +1,17 @@
+/*
+Author: Talan Tabares
+StudentID: 3108074
+Date: 3/26/2025
+Reason: Lab07
+Description:
+Program that performs temperature conversions in Fahrenheir, Celsius, and Kelvin. This program will also categorize each temperature into a weather advisory category
+
+inputs:
+
+
+outputs:
+
+
+*/
+
+
